4598109a70a7771fdf67469787ce2bc232b9a976
Peter Jannesen <peter@jannesen.com>
2019-10-27 21:35:52 +0100

    Bump up version to 1.09.00.001

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct          ("Jannesen Tools")]
 [assembly: AssemblyTrademark        ("")]
 [assembly: AssemblyCulture          ("")]
-[assembly: AssemblyVersion          ("1.09.00.000")] /*@VERSIONINFO*/
-[assembly: AssemblyFileVersion      ("1.09.00.000")] /*@VERSIONINFO*/
+[assembly: AssemblyVersion          ("1.09.00.001")] /*@VERSIONINFO*/
+[assembly: AssemblyFileVersion      ("1.09.00.001")] /*@VERSIONINFO*/
 [assembly: ComVisible               (false)]
