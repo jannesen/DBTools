30460bc0dd9c764662ee1fac82997746bcdbf343
Peter Jannesen <peter@jannesen.com>
2026-06-12 19:00:15 +0200

    Build fixes

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ $dotnet='C:\Program Files\dotnet\dotnet.exe'
 
 if (-not (Test-Path -Path $dotnet  -PathType Leaf)) { throw "Can't find dotnet"  }
 
-function Cleanup() {
+function Cmd-Cleanup() {
     Write-Host '# cleanup'
 
     foreach ($proj in (Get-ChildItem -Path . -Recurse -File -Filter "*.csproj")) {
@@ -28,14 +28,31 @@ function Cleanup() {
 }
 
 function Cmd-Publish() {
-    Cleanup
+    Cmd-Cleanup
 
     Write-Host '# dotnet publish win-x64'
     & $dotnet publish DBTools.slnx --verbosity minimal -c Release -r win-x64 /p:DebugSymbols=false
 }
 
+function Cmd-Install() {
+    $path="$(Split-Path -Path $(Get-Location))\bin\DBTools.exe"
+    $target="$(Get-Location)\output\DBTools.exe" 
+    
+    if (-not (Test-Path $target -PathType Leaf)) {
+        Cmd-Publish
+    }
+        
+    Write-Host "# create symlink $path to $target"
+    
+    if (Test-Path $path -PathType Leaf) {
+        Remove-Item $path 
+    }
+
+    New-Item -Path $path -ItemType SymbolicLink -Target $target | Out-Null
+}
+
 if ($args.Length -eq 0) {
-    Write-Host "SYNTAX: builder publish|cleanup"
+    Write-Host "SYNTAX: builder publish|install|cleanup"
     Exit 1
 }
 
@@ -52,8 +69,13 @@ while ($argn -lt $args.Length) {
             $argn = $argn + 1
         }
 
+    "install" {
+            Cmd-Install
+            $argn = $argn + 1
+        }
+
     "cleanup" {
-            Cleanup
+            Cmd-Cleanup
             $argn = $argn + 1
         }        
 
